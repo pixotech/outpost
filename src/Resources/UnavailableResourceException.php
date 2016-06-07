ba57f80da6cc3d0c490bc6cb1c9e7cd7aa178c23
Matt Sharkey <matt@pixotech.com>
2016-06-07 17:48:35 -0500

    Add help for unavailable resource exception

--- src/Resources/UnavailableResourceException.php
+++ src/Resources/UnavailableResourceException.php
@@ -2,28 +2,53 @@
 
 namespace Outpost\Resources;
 
-class UnavailableResourceException extends \Exception {
+use Outpost\Exceptions\Exception;
 
-  /**
-   * @var callable
-   */
-  protected $resource;
+class UnavailableResourceException extends Exception
+{
 
-  /**
-   * @param callable $resource
-   * @param \Exception $exception
-   * @param null $message
-   */
-  public function __construct(callable $resource, \Exception $exception = null, $message = null) {
-    if (empty($message)) $message = $exception->getMessage() ?: "Resource unavailable";
-    parent::__construct($message, 0, $exception);
-    $this->resource = $resource;
-  }
+    /**
+     * @var callable
+     */
+    protected $resource;
 
-  /**
-   * @return callable
-   */
-  public function getResource() {
-    return $this->resource;
-  }
+    /**
+     * @param callable $resource
+     * @param \Exception $exception
+     * @param null $message
+     */
+    public function __construct(callable $resource, \Exception $exception = null, $message = null)
+    {
+        if (empty($message)) {
+            $message = $exception->getMessage() ?: "Resource unavailable";
+        }
+        parent::__construct($message, 0, $exception);
+        $this->resource = $resource;
+    }
+
+    public function getHelp()
+    {
+        return <<<HTML
+
+<pre>{$this->getResourceClassname()}</pre>
+
+<p>This resource was unavailable.</p>
+
+<p>It was requested on line {$this->getLine()} of <code>{$this->getFile()}</code>.</p>
+
+HTML;
+    }
+
+    /**
+     * @return callable
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    public function getResourceClassname()
+    {
+        return get_class($this->getResource());
+    }
 }
