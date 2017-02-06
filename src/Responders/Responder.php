f1bb61df50e456063edc366565a131f4ef8aa45f
Matt Sharkey <matt@pixotech.com>
2017-02-06 07:57:31 -0600

    Responders

--- src/Responders/Responder.php
+++ src/Responders/Responder.php
@@ -4,28 +4,61 @@ namespace Outpost\Responders;
 
 use Outpost\SiteInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Responder implements ResponderInterface
 {
     protected $tasks = [];
 
-    public function __invoke(SiteInterface $site, Request $request, $response = null)
+    /**
+     * @param SiteInterface $site
+     * @param Request $request
+     * @return mixed
+     */
+    public function __invoke(SiteInterface $site, Request $request)
     {
-        foreach ($this->tasks as $task) {
-            $response = call_user_func($task, $site, $request, $response);
-        }
-        return $response;
+        return $this->respond($site, $request);
     }
 
+    /**
+     * @param callable $resource
+     * @return $this
+     */
     public function get(callable $resource)
     {
         $this->tasks[] = new ResourceTask($resource);
         return $this;
     }
 
+    /**
+     * @param $template
+     * @return $this
+     */
     public function render($template)
     {
         $this->tasks[] = new RenderTask($template);
         return $this;
+    }
+
+    /**
+     * @param SiteInterface $site
+     * @param Request $request
+     * @return mixed
+     */
+    public function respond(SiteInterface $site, Request $request)
+    {
+        return $this->makeResponse($site, $request);
+    }
+
+    /**
+     * @param SiteInterface $site
+     * @param Request $request
+     * @return Response
+     */
+    protected function makeResponse(SiteInterface $site, Request $request)
+    {
+        return array_reduce($this->tasks, function ($response, callable $task) use ($site, $request) {
+            return call_user_func($task, $site, $request, $response);
+        });
     }
 }
